c8b8519fac9c2999ba41810633f6933980663362
minhhoccode111 <minhhoccode111@gmail.com>
2025-07-13 22:22:59 +0700

    udemy flutter: first app

---
+++ udemy_flutter/README.md
@@ -0,0 +1,1 @@
+# [Udemy Flutter](https://eylearning.udemy.com/course/learn-flutter-dart-to-build-ios-android-apps/learn/)
---
+++ udemy_flutter/first_app/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.deepPurple,
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 26, 2, 80),
+                Color.fromARGB(255, 45, 7, 98),
+              ],
+            ),
+          ),
+          child: const Center(child: Text('Hello, World!')),
+        ),
+      ),
+    ),
+  );
+}
